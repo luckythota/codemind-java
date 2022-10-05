348170188a86e3c9c0cf8124e9481b88a755d404
luckythota <97425859+luckythota@users.noreply.github.com>
2022-10-05 21:42:53 +0530

    import java.util.Scanner; class sumof {     public static void main(String args[])     {         int n,v;         Scanner sc=new Scanner(System.in);         n=sc.nextInt();         v=n*(n+1)/2;         System.out.println(v);
        }
    }

---
+++ Find_sum_of_natural_numbers_.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+class sumof
+{
+    public static void main(String args[])
+    {
+        int n,v;
+        Scanner sc=new Scanner(System.in);
+        n=sc.nextInt();
+        v=n*(n+1)/2;
+        System.out.println(v);
+        
+    }
+}
