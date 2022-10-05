40eb69b88bc8e6c35fa0df159d7de9be63a955e5
luckythota <97425859+luckythota@users.noreply.github.com>
2022-10-05 19:47:36 +0530

    import java.util.Scanner; class grades {    public static void main(String args[])    {       int h,s,sp;       Scanner sc = new Scanner(System.in);       h=sc.nextInt();       s=sc.nextInt();       sp=sc.nextInt();       if(h>50 && s>60 && sp>100)       {           System.out.println("10");       }       else if(h>50 && s>60)       {          System.out.println("9");       }       else if(s>60 && sp>100)       {           System.out.println("8");       }       else if(h>50 && sp>100)       {           System.out.println("7");       }       else if(h>50 || s>60 || sp>100)       {           System.out.println("6");       }       else       {           System.out.println("5");       }    } }

---
+++ Grades_of_Rides.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+class grades 
+{
+   public static void main(String args[])
+   {
+      int h,s,sp;
+      Scanner sc = new Scanner(System.in);
+      h=sc.nextInt();
+      s=sc.nextInt();
+      sp=sc.nextInt();
+      if(h>50 && s>60 && sp>100)
+      {
+          System.out.println("10");
+      }
+      else if(h>50 && s>60)
+      {
+         System.out.println("9");
+      }
+      else if(s>60 && sp>100)
+      {
+          System.out.println("8");
+      }
+      else if(h>50 && sp>100)
+      {
+          System.out.println("7");
+      }
+      else if(h>50 || s>60 || sp>100)
+      {
+          System.out.println("6");
+      }
+      else
+      {
+          System.out.println("5");
+      }
+   }
+}
