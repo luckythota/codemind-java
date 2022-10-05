dd17ea80cf0e48f3198a48f93f0fdc615842efcf
luckythota <97425859+luckythota@users.noreply.github.com>
2022-10-05 19:59:48 +0530

    import java.util.Scanner; class grades {    public static void main(String args[])    {       char n;       Scanner sc = new Scanner(System.in);       n=sc.next().charAt(0);       if(n=='V' || n=='v')       {           System.out.println("Violet");       }       else if(n=='I' || n=='i')       {          System.out.println("Indigo");       }       else if(n=='B' || n=='b')       {           System.out.println("Blue");       }       else if(n=='G' || n=='g')       {           System.out.println("Green");       }       else if(n=='Y' || n=='y')       {           System.out.println("yellow");       }       else if(n=='O' || n=='o')       {           System.out.println("Orange");       }       else if(n=='R' || n=='r')       {           System.out.println("Red");       }       else       {           System.out.println("-1");       }    } }

---
+++ Color_Code.java
@@ -0,0 +1,42 @@
+import java.util.Scanner;
+class grades 
+{
+   public static void main(String args[])
+   {
+      char n;
+      Scanner sc = new Scanner(System.in);
+      n=sc.next().charAt(0);
+      if(n=='V' || n=='v')
+      {
+          System.out.println("Violet");
+      }
+      else if(n=='I' || n=='i')
+      {
+         System.out.println("Indigo");
+      }
+      else if(n=='B' || n=='b')
+      {
+          System.out.println("Blue");
+      }
+      else if(n=='G' || n=='g')
+      {
+          System.out.println("Green");
+      }
+      else if(n=='Y' || n=='y')
+      {
+          System.out.println("yellow");
+      }
+      else if(n=='O' || n=='o')
+      {
+          System.out.println("Orange");
+      }
+      else if(n=='R' || n=='r')
+      {
+          System.out.println("Red");
+      }
+      else
+      {
+          System.out.println("-1");
+      }
+   }
+}
