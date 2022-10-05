beebda180dec642518be118b6cf612f3ceca6847
luckythota <97425859+luckythota@users.noreply.github.com>
2022-10-05 20:24:30 +0530

    import java.util.Scanner; class multi {    public static void main(String args[])    {        float m,n,v;        Scanner sc=new Scanner(System.in);        m=sc.nextFloat();        n=sc.nextFloat();        v=m*n;        System.out.format("%.2f",v);
       }
    }

---
+++ Program_to_Multiply_Two_Floating-Point_Numbers.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+class multi
+{
+   public static void main(String args[])
+   {
+       float m,n,v;
+       Scanner sc=new Scanner(System.in);
+       m=sc.nextFloat();
+       n=sc.nextFloat();
+       v=m*n;
+       System.out.format("%.2f",v);
+       
+   }
+}
