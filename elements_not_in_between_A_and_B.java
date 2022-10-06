60b9b2a4fb94e370a34a8f0e2fe3903d7a9c268d
luckythota <97425859+luckythota@users.noreply.github.com>
2022-10-06 11:12:27 +0530

    import java.util.Scanner; class element {     public static void main(String args[])     {         int n,i,k,v,flag=0;         Scanner sc=new Scanner(System.in);         n=sc.nextInt();         int [] a=new int[n];         for(i=0; i<n; i++)         {             a[i]=sc.nextInt();         }         k=sc.nextInt();         v=sc.nextInt();         for(i=0; i<n; i++)         {             if(a[i]>=k && a[i]<=v)             {                 continue;             }             else             {                 System.out.print(a[i]+" ");                 flag=1;             }         }         if(flag==0)         {             System.out.print("-1");         }
        }
    }

---
+++ elements_not_in_between_A_and_B.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+class element
+{
+    public static void main(String args[])
+    {
+        int n,i,k,v,flag=0;
+        Scanner sc=new Scanner(System.in);
+        n=sc.nextInt();
+        int [] a=new int[n];
+        for(i=0; i<n; i++)
+        {
+            a[i]=sc.nextInt();
+        }
+        k=sc.nextInt();
+        v=sc.nextInt();
+        for(i=0; i<n; i++)
+        {
+            if(a[i]>=k && a[i]<=v)
+            {
+                continue;
+            }
+            else
+            {
+                System.out.print(a[i]+" ");
+                flag=1;
+            }
+        }
+        if(flag==0)
+        {
+            System.out.print("-1");
+        }
+
+    }
+}
