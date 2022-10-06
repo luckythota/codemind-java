db1be4698ef7997cdaeb5f3d3cda8f2267f93f86
luckythota <97425859+luckythota@users.noreply.github.com>
2022-10-06 11:19:30 +0530

    import java.util.Scanner; class element {     public static void main(String args[])     {         int n,i,flag=0;         Scanner sc=new Scanner(System.in);         n=sc.nextInt();         int [] a=new int[n];         for(i=0; i<n; i++)         {             a[i]=sc.nextInt();         }         for(i=0; i<n; i++)         {             if(a[i]%2!=0)             {                 System.out.print(a[i]+" ");             }         }         for(i=0; i<n; i++)         {             if(a[i]%2==0)             {                 System.out.print(a[i]+" ");             }         }
        }
    }

---
+++ array_of_add_and_even_elements.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+class element
+{
+    public static void main(String args[])
+    {
+        int n,i,flag=0;
+        Scanner sc=new Scanner(System.in);
+        n=sc.nextInt();
+        int [] a=new int[n];
+        for(i=0; i<n; i++)
+        {
+            a[i]=sc.nextInt();
+        }
+        for(i=0; i<n; i++)
+        {
+            if(a[i]%2!=0)
+            {
+                System.out.print(a[i]+" ");
+            }
+        }
+        for(i=0; i<n; i++)
+        {
+            if(a[i]%2==0)
+            {
+                System.out.print(a[i]+" ");
+            }
+        }
+
+    }
+}
