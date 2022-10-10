d0d113fd4ee3236faf798a75468dfd8f84a63240
luckythota <97425859+luckythota@users.noreply.github.com>
2022-10-10 10:39:47 +0530

    import java.util.Scanner; class electricity {     public static void main(String args[])     {         String s;         Scanner sc=new Scanner(System.in);         s=sc.nextLine();         System.out.print("Hello Technicalhub ");         System.out.print(s);     } }

---
+++ Hello_Technicalhub.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+class electricity
+{
+    public static void main(String args[])
+    {
+        String s;
+        Scanner sc=new Scanner(System.in);
+        s=sc.nextLine();
+        System.out.print("Hello Technicalhub
+");
+        System.out.print(s);
+    }
+}
