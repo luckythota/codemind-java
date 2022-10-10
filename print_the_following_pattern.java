d79cb2c5e90b1b182445379a8b194a0ee5cf34d9
luckythota <97425859+luckythota@users.noreply.github.com>
2022-10-10 21:50:51 +0530

    import java.util.Scanner; class pattern {     public static void main(String args[])     {         int n,i,j;         Scanner sc=new Scanner(System.in);         n=sc.nextInt();         for(i=1; i<=n; i++)         {             for(j=1; j<=n; j++)             {                 if(i==j)                 {                     System.out.print("0"+"");                 }                 else                 {                     System.out.print("x"+"");                 }             }                 System.out.println();         }
        }
    }

--- print_the_following_pattern.java
+++ print_the_following_pattern.java
@@ -6,13 +6,20 @@ class pattern
         int n,i,j;
         Scanner sc=new Scanner(System.in);
         n=sc.nextInt();
-        for(i=n; i>=1; i--)
+        for(i=1; i<=n; i++)
         {
-            for(j=1; j<=i; j++)
+            for(j=1; j<=n; j++)
             {
-                System.out.print(j+"");
+                if(i==j)
+                {
+                    System.out.print("0"+"");
+                }
+                else
+                {
+                    System.out.print("x"+"");
+                }
             }
-            System.out.println();
+                System.out.println();
         }
         
         
