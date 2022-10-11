f2120370a307784431c6f82f07460610542879a8
luckythota <97425859+luckythota@users.noreply.github.com>
2022-10-11 15:45:59 +0530

    import java.util.Scanner; class sum {     public static void main(String args[])     {         int n,i,pro=1,ac=0,bc=0;         Scanner sc=new Scanner(System.in);         n=sc.nextInt();         int []a=new int[n];         for(i=0; i<n; i++)         {             a[i]=sc.nextInt();         }         for(i=0; i<n; i++)         {             if(a[i]!=0)             {                 System.out.print(a[i]+" ");             }         }         for(i=0; i<n; i++)         {             if(a[i]==0)             {               System.out.print(a[i]+" ");             }         }
        }
    }

---
+++ Move_Zeroes.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+class sum
+{
+    public static void main(String args[])
+    {
+        int n,i,pro=1,ac=0,bc=0;
+        Scanner sc=new Scanner(System.in);
+        n=sc.nextInt();
+        int []a=new int[n];
+        for(i=0; i<n; i++)
+        {
+            a[i]=sc.nextInt();
+        }
+        for(i=0; i<n; i++)
+        {
+            if(a[i]!=0)
+            {
+                System.out.print(a[i]+" ");
+            }
+        }
+        for(i=0; i<n; i++)
+        {
+            if(a[i]==0)
+            {
+              System.out.print(a[i]+" ");
+            }
+        }
+        
+    }
+}
